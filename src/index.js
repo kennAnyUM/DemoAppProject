339940bc2f52e57e4d16f6d62f97b4d08f764b39
Ronny230797 <ronny230797@gmail.com>
2022-01-15 11:05:30 -0800

    Added material UI library.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ExampleShopping from './shopping/shopping'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <ExampleShopping />
   </React.StrictMode>,
   document.getElementById('root')
 );
